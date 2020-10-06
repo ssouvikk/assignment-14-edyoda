4c3096db9125b7b6dcc8f5cba68fec15a33e4cba
Souvik Mondal <63383351+ssouvikk@users.noreply.github.com>
2020-10-06 19:26:15 +0530

    after build

---
+++ build/precache-manifest.28a50e753b6133dea066f54473b9a8e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94a206a59d9bf1cea20dbc76084a995a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2472d09dc12b9c0c516b",
+    "url": "/static/css/2.e6ac38db.chunk.css"
+  },
+  {
+    "revision": "6301f0fd9b9ce117ef41",
+    "url": "/static/css/main.f1854329.chunk.css"
+  },
+  {
+    "revision": "2472d09dc12b9c0c516b",
+    "url": "/static/js/2.37522817.chunk.js"
+  },
+  {
+    "revision": "13f39c7b3d7eb1a92726d49343d62428",
+    "url": "/static/js/2.37522817.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6301f0fd9b9ce117ef41",
+    "url": "/static/js/main.47230941.chunk.js"
+  },
+  {
+    "revision": "6bb790250bac5418aa46",
+    "url": "/static/js/runtime-main.9b1c7b60.js"
+  }
+]);
